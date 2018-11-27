2ca117aabf13a3f60250cd8dfc360bd462a6fd50
Maximilian Werzinger <maximilian.werzinger@googlemail.com>
2018-11-27 15:38:35 +0100

    added all CRUD-Functions

--- src/main/java/people/boundary/PeopleResource.java
+++ src/main/java/people/boundary/PeopleResource.java
@@ -4,8 +4,12 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,11 +25,42 @@ public class PeopleResource {
 	@PersistenceContext
 	private EntityManager em;
 
+	@GET
+	@Path("{id}")
+	public Response getPeopleById(@PathParam("id") Integer peopleId) {
+		return Response //
+				.status(200) //
+				.entity(em.find(People.class, peopleId)) //
+				.build();
+	}
+
 	@POST
 	public Response addNewPeople(People people) {
 		em.persist(people);
 		em.flush();
-		return Response.status(200).entity(people).build();
+		return Response //
+				.status(200) //
+				.entity(people) //
+				.build();
+	}
+
+	@PUT
+	public Response updateExistingPeople(People peopleToUpdate) {
+		return Response //
+				.status(200) //
+				.entity(em.merge(peopleToUpdate)) //
+				.build();
+	}
+
+	@DELETE
+	@Path("/delete/{id}")
+	public Response deletePeopleById(@PathParam("id") Integer peopleId) {
+		People peopleToDelete = em.find(People.class, peopleId);
+		em.remove(peopleToDelete);
+		return Response //
+				.status(200) //
+				.entity(peopleToDelete) //
+				.build();
 	}
 
 }
